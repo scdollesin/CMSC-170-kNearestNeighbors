55abc65242807a5c9e3d5292568b89f79fc44343
scdollesin <scdollesin@up.edu.ph>
2023-11-07 01:48:41 +0800

    feat: determine the k nearest neighbors

--- knn.py
+++ knn.py
@@ -26,18 +26,27 @@ if (trainingfile.readable()):
     inputfile = open(askopenfilename(), "r")
     if (inputfile.readable()):
         unlabelled_points = getData(inputfile)
+
         #ask user for value of k
         k = int(input("Enter k: "))
-        nearest_neighbors = [0]*k   #index of the k-nearest neighbors
-        print(nearest_neighbors)
 
         n = len(unlabelled_points[1])     #no. of dimensions
+        all_distances = {}
 
         for i in range(len(unlabelled_points)):
             for j in range(len(training_dataset)):
+                #compute for distance
                 distance = 0
                 for h in range(n):
                     distance = distance + (unlabelled_points[i][h] - training_dataset[j][h])**2
                 distance = sqrt(distance)
+                all_distances[j] = distance
+
                 if (i==1) and (j==1):
                     print(distance)
+            
+        sorted_dist = dict(sorted(all_distances.items(), key=lambda item: item[1]))
+        nearest_neighbors = list(sorted_dist.keys())[:k]    #contains the index of the k-nearest neighbors
+
+        print(nearest_neighbors)
+
